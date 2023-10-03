953e0af2c19b6ccba139434ebc9ce5e6d7cfa0f2
jjsyp <jsypniew@emich.edu>
2023-10-03 16:16:21 -0400

    added a word or two

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World.");
-		System.out.println("alternate");
+		System.out.println("alternate, added a word or two");
 	}
 
 }
